a22358d7a4714e65f384b6364f955e3e0f3fc2b8
Naofumi Kagami <naofumi@mac.com>
2013-12-02 03:26:30 +0900

    Helper to allow only KSP links on email

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,11 +1,23 @@
 module MailerHelper
   def ksp_and_galapagos_email_links(*ksp_args)
     raise "must set @bootloader_path" unless @bootloader_path
+    "#{ksp_email_link(*ksp_args)}\n(imode: #{galapagos_email_link(*ksp_args)})"
+  end
+
+  def ksp_email_link(*ksp_args)
+    raise "must set @bootloader_path" unless @bootloader_path
     ksp_link_args = ksp_args.dup.push({:force_kamishibai => true, :bootloader_path => @bootloader_path})
-    galapagos_link_args = ksp_args.dup.push({:force_galapagos => true, :bootloader_path => @bootloader_path})
     ksp_link = ksp(*ksp_link_args)
+  end
+
+  def galapagos_email_link(*ksp_args)
+    raise "must set @bootloader_path" unless @bootloader_path
+    galapagos_link_args = ksp_args.dup.push({:force_galapagos => true, :bootloader_path => @bootloader_path})
     galapagos_link = ksp(*galapagos_link_args)
-    "#{ksp_link}\n(imode: #{galapagos_link})"
+  end
+
+  def linkify(url)
+    "<a href=\"#{url}\">#{url}</a>"
   end
 
   # options :text => true/false
