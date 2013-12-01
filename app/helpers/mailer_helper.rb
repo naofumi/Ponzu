102e659ab7c0b2f08d56345f237ef4b3ef48ba2b
Naofumi Kagami <naofumi@mac.com>
2013-12-02 03:33:35 +0900

    Fix design of notification emails

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -24,4 +24,18 @@ module MailerHelper
   def mail_footer(options = {})
     t('message_mailer.footer', :namespace => @conference.tag)
   end
+
+  def header_image
+    result = []
+    result << '<div style="text-align:center">'
+    result << image_tag("#{@bootloader_path}/" + image_path("#{@conference.tag}/apple-touch-icons/apple-touch-icon.png"), 
+                        :alt => "#{@conference.tag}-icon")
+    result << '</div>'
+    result.join("\n").html_safe
+  end
+
+  def body_style
+    "border-radius: 5px;border: solid 1px #AAA; padding: 10px;margin: 20px 0;"
+  end
+
 end
