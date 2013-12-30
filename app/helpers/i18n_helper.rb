87b8b07ce9bf259fb282e48e4187edd25d7b26e4
Naofumi Kagami <naofumi@mac.com>
2013-12-30 23:52:09 +0900

    Use spaces for indents instead of tabs

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -1,14 +1,14 @@
 module I18nHelper
-	def t(*args)
-		if args.last.kind_of? Hash
-			args.last.merge!(:namespace => current_conference.tag) unless args.last[:namespace]
-		else
-			if respond_to? :current_conference
-				args.push({:namespace => current_conference.tag})
-			else
-				raise "Must specify :namespace in #t unless respond to #current_conference"
-			end
-		end
-		translate(*args)
-	end
+  def t(*args)
+    if args.last.kind_of? Hash
+      args.last.merge!(:namespace => current_conference.tag) unless args.last[:namespace]
+    else
+      if respond_to? :current_conference
+        args.push({:namespace => current_conference.tag})
+      else
+        raise "Must specify :namespace in #t unless respond to #current_conference"
+      end
+    end
+    translate(*args)
+  end
 end
