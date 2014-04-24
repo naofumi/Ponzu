cb349d030053625f80bd1c889391c5ad984aa392
Naofumi Kagami <naofumi@mac.com>
2014-04-24 13:05:49 +0900

    Added comments on Grid

--- app/models/grid.rb
+++ app/models/grid.rb
@@ -9,6 +9,14 @@
 # Presentation::Mappable#grid). Eveery Presenation::Mappable
 # object must implement a Presentation::Mappable#grids method
 # which returns an array of Grid objects.
+#
+# For legacy reasons, Grid attributes are named :x and :y.
+# y: a row of posters
+# x: the position of the poster within the row
+#
+# We usually index from 0
+#
+# Group is normally indexed from 1
 class Grid < Struct.new(:x, :y, :group)
   def initialize(x, y, group = 0)
     super(x, y, group)
