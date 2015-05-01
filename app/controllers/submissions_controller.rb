49fff8d494e81a07a354078464670bcfdd780efd
Naofumi Kagami <naofumi@mac.com>
2015-05-02 08:00:20 +0900

    Fix bug in submission.skip_authorships_and_institutions_validations version 2.4.3

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -53,7 +53,7 @@ class SubmissionsController < ApplicationController
     @submission = Submission.new(params[:submission])
     verify_ownership(@submission)
     @submission.conference_confirm = current_conference
-    @submission.skip_authorships_and_institutions_validations
+    @submission.skip_authorships_and_institutions_validations == true
     # success = false
     Submission.transaction do
       begin
